4311db244d902b2f43ef9fa3ae31c71c3bdbdcae
Oleg Veremei <veremeioleg@gmail.com>
2024-01-28 11:01:37 +0300

    added Verify error after opening the articles mobile test

--- src/test/java/by/veremei/pages/MainPage.java
+++ src/test/java/by/veremei/pages/MainPage.java
@@ -1,13 +1,28 @@
 package by.veremei.pages;
 
 import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
+import static io.appium.java_client.AppiumBy.id;
 import static io.appium.java_client.MobileBy.AccessibilityId;
 
 public class MainPage {
-    private final SelenideElement searchButtonLocator = $(AccessibilityId("Search Wikipedia"));
+    private final SelenideElement searchButtonLocator = $(AccessibilityId("Search Wikipedia")),
+                                  firstArticleDescriptionLocator = $(id("org.wikipedia.alpha:id/page_list_item_description")),
+                                  wikiErrorTextLocator = $(id("org.wikipedia.alpha:id/view_wiki_error_text")),
+                                  wikiErrorBtnLocator = $(id("org.wikipedia.alpha:id/view_wiki_error_button"));
 
     public void clickSearchButton() {
         $(searchButtonLocator).click();
+    }
+
+    public void clickFirstArticle() {
+        firstArticleDescriptionLocator.click();
+    }
+
+    public void verifyErrorAfterOpeningArticle(String error) {
+        wikiErrorTextLocator.shouldHave(text(error));
+        wikiErrorBtnLocator.click();
     }
 }
--- src/test/java/by/veremei/test/SearchTest.java
+++ src/test/java/by/veremei/test/SearchTest.java
@@ -12,7 +12,8 @@ import static io.qameta.allure.Allure.step;
 public class SearchTest extends TestBase {
     private final SearchPage searchPage = new SearchPage();
     private final MainPage mainPage = new MainPage();
-    private final static String SEARCH_WORD = "Appium";
+    private final static String SEARCH_WORD = "Appium",
+                                WIKI_ERROR_TEXT = "Error";
     @Test
     @DisplayName("Check search result")
     void successfulSearchTest() {
@@ -22,5 +23,21 @@ public class SearchTest extends TestBase {
         });
         step("Verify content found", () ->
             searchPage.getSearchResultsCount().shouldHave(sizeGreaterThan(0)));
+    }
+
+    @Test
+    @DisplayName("Verify error after opening the articles")
+    void openPageTest() {
+        step("Type search", () -> {
+            mainPage.clickSearchButton();
+            searchPage.enterSearchText(SEARCH_WORD);
+        });
+        step("Verify content found", () ->
+            searchPage.getSearchResultsCount().shouldHave(sizeGreaterThan(0)));
+        step("Open page with article", mainPage::clickFirstArticle
+        );
+        step("Verify error after opening the articles", () ->
+            mainPage.verifyErrorAfterOpeningArticle(WIKI_ERROR_TEXT)
+        );
     }
 }
